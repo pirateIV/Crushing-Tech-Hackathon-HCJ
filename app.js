b38a7a7d69e797dfb26f1b343bae063ac2ebbaf1
ben64103 <benabolade@gmail.com>
2023-11-21 19:22:19 +0100

    working on main section

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 // Your javascript goes here
 const userSelect = document.querySelector('#user-select');
 const dropdown = document.querySelector('#dropdown');
-const showAlert = document.querySelector('#alertsBtn')
+const showAlert = document.querySelector('#showAlertBtn');
+const alertDropdown = document.querySelector('#alertDropdown')
 
+let delayDuration = 100;
 const showDropdown = () => {
-  let delayDuration = 100;
   setTimeout(() => {
     dropdown.hidden ? (dropdown.hidden = false) : (dropdown.hidden = true);
-    showAlert.hidden ? (showAlert.hidden = false) : (showAlert.hidden = true)
+
   }, delayDuration);
 };
+const showAlertDropdown = () => {
+  setTimeout(() => {
+    alertDropdown.hidden ? (alertDropdown.hidden = false) : (alertDropdown.hidden = true);
+  }, delayDuration )
+  
+}
 userSelect.addEventListener('click', showDropdown);
-userSelect.addEventListener('keydown', (e) => {
-  if(e.target.key === 'Enter') {
-    showDropdown()
-  }
-})
+showAlert.addEventListener('click', showAlertDropdown);
