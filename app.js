5477ef3db1d1fff9c197ded13195a1ff9483188b
ben64103 <benabolade@gmail.com>
2023-11-21 14:08:32 +0100

    working on alerts dropdown

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // Your javascript goes here
 const userSelect = document.querySelector('#user-select');
 const dropdown = document.querySelector('#dropdown');
-
+const showAlert = document.querySelector('#alertsBtn')
 
 const showDropdown = () => {
   let delayDuration = 100;
   setTimeout(() => {
     dropdown.hidden ? (dropdown.hidden = false) : (dropdown.hidden = true);
+    showAlert.hidden ? (showAlert.hidden = false) : (showAlert.hidden = true)
   }, delayDuration);
 };
 userSelect.addEventListener('click', showDropdown);
+userSelect.addEventListener('keydown', (e) => {
+  if(e.target.key === 'Enter') {
+    showDropdown()
+  }
+})
