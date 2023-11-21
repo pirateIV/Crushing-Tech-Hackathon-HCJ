3e5ec6302b05dda05556a0e42a382be3051670e1
ben64103 <benabolade@gmail.com>
2023-11-21 13:05:17 +0100

    fix error: dropdown

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-
 // Your javascript goes here
+const userSelect = document.querySelector('#user-select');
+const dropdown = document.querySelector('#dropdown');
+
+
+const showDropdown = () => {
+  let delayDuration = 100;
+  setTimeout(() => {
+    dropdown.hidden ? (dropdown.hidden = false) : (dropdown.hidden = true);
+  }, delayDuration);
+};
+userSelect.addEventListener('click', showDropdown);
