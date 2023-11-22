fa9043f6d2d32c5c17d295c74aa31997e90d19d0
ben64103 <benabolade@gmail.com>
2023-11-22 03:10:40 +0100

    working on dropdowns

--- app.js
+++ app.js
@@ -2,20 +2,33 @@
 const userSelect = document.querySelector('#user-select');
 const dropdown = document.querySelector('#dropdown');
 const showAlert = document.querySelector('#showAlertBtn');
-const alertDropdown = document.querySelector('#alertDropdown')
+const alertDropdown = document.querySelector('#alertDropdown');
 
 let delayDuration = 100;
+
 const showDropdown = () => {
   setTimeout(() => {
-    dropdown.hidden ? (dropdown.hidden = false) : (dropdown.hidden = true);
-
+    if (alertDropdown.classList.contains('flex')) {
+      alertDropdown.classList.toggle(
+        alertDropdown.classList.contains('flex') ? 'flex' : 'hide'
+      );
+    }
+    dropdown.classList.toggle(
+      dropdown.classList.contains('hide') ? 'block' : 'hide'
+    );
   }, delayDuration);
 };
 const showAlertDropdown = () => {
   setTimeout(() => {
-    alertDropdown.hidden ? (alertDropdown.hidden = false) : (alertDropdown.hidden = true);
-  }, delayDuration )
-  
-}
+    if (dropdown.classList.contains('block')) {
+      dropdown.classList.toggle(
+        dropdown.classList.contains('block') ? 'block' : 'hide'
+      );
+    }
+    alertDropdown.classList.toggle(
+      alertDropdown.classList.contains('hide') ? 'flex' : 'hide'
+    );
+  }, delayDuration);
+};
 userSelect.addEventListener('click', showDropdown);
 showAlert.addEventListener('click', showAlertDropdown);
